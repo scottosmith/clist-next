f16b7c0ccda143fbd062a701bd858ebe899be2ce
Scott Smith <scott@scottosmith.net>
2020-12-06 17:27:51 -0600

    Rough draft

---
+++ pages/api/search/[...params].js
@@ -0,0 +1,26 @@
+import Cors from "cors";
+import initMiddleware from "../../../lib/init-middleware";
+
+const cors = initMiddleware(
+  Cors({
+    methods: ["GET"],
+    origin: true,
+  })
+);
+
+export default async function handler(req, res) {
+  try {
+    await cors(req, res);
+    const {
+      query: { params },
+    } = req;
+
+    const [area, cat, query] = params;
+    const url = `https://${area}.craigslist.org/search/${cat}?query=${query}&hasPic=1`;
+    const response = await fetch(url);
+    const text = await response.text();
+    res.status(200).json({ html: text });
+  } catch (error) {
+    throw error;
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,63 @@
-export default function Index() {
+import DOMParser from "dom-parser";
+import { useState } from "react";
+
+function Index() {
+  const [searchResults, setSearchResults] = useState(null);
+
+  const fetchData = async () => {
+    const req = await fetch("/api/search/stlouis/pho/nikon");
+    const newData = await req.json();
+
+    if (newData) {
+      let parser = new DOMParser();
+      const html = parser.parseFromString(newData.html, "text/html");
+      const htmlByClass = html.getElementsByClassName("rows");
+
+      let results = [];
+      for (let match of htmlByClass[0].childNodes) {
+        if (match.nodeName === "li") {
+          results.push(match);
+        }
+      }
+
+      if (results.length) {
+        const htmlResults = results.map((result) => {
+          const imgIds = result.childNodes[1].attributes[2].value.split(",");
+          const imgUrl = `https://images.craigslist.org/${imgIds[0].slice(
+            2
+          )}_300x300.jpg`;
+          const title = result.getElementsByClassName("result-title hdrlnk")[0]
+            .innerHTML;
+          const price = result.getElementsByClassName("result-price")[0]
+            .innerHTML;
+          const postUrl = result.getElementsByClassName(
+            "result-image gallery"
+          )[0].href;
+          return (
+            <div key={Math.random()}>
+              <img src={imgUrl} />
+              <span>
+                {title} - {price}
+              </span>
+              <a href={postUrl}>{title}</a>
+            </div>
+            // <img
+            //   key={imgIds[0].slice(2)}
+            //   src={`https://images.craigslist.org/${imgIds[0].slice(2)}_300x300.jpg`}
+            // />
+          );
+        });
+        setSearchResults(htmlResults);
+      }
+    }
+  };
+
   return (
-    <p>
-      To test the CORS route, open the console in a new tab on a different
-      domain and make a POST / GET / OPTIONS request to <b>/api/cors</b>. Using
-      a different method from those mentioned will be blocked by CORS
-    </p>
-  )
+    <>
+      <button onClick={fetchData}>Get Data</button>
+      {searchResults ? searchResults.map((result) => result) : <p>nothing</p>}
+    </>
+  );
 }
+
+export default Index;
